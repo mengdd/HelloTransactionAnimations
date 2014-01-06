b8fad7877dcf582b80afaacbf7345c37e178734c
mengdandan <mengdd23@gmail.com>
2014-01-06 13:37:36 +0800

    merge

--- src/com/mengdd/transaction/activity/leftfade/FirstActivity.java
+++ src/com/mengdd/transaction/activity/leftfade/FirstActivity.java
@@ -4,6 +4,7 @@ import com.mengdd.transactionani.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +15,11 @@ public class FirstActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leftfade_layout);
+
+        View decorView = getWindow().getDecorView();
+//        decorView.setBackgroundColor(Color.RED);
+//        decorView.setBackgroundColor(Color.BLACK);
+        decorView.setBackgroundColor(Color.WHITE);
 
         View nextButton = findViewById(R.id.next);
         nextButton.setOnClickListener(new OnClickListener() {
