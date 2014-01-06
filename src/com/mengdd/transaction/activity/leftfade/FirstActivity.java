9979bc3cd772586fa86ced5afaf23989d4694a7e
mengdandan <mengdd23@gmail.com>
2014-01-06 12:41:30 +0800

    left face activity animation

--- src/com/mengdd/transaction/activity/leftfade/FirstActivity.java
+++ src/com/mengdd/transaction/activity/leftfade/FirstActivity.java
@@ -3,7 +3,10 @@ package com.mengdd.transaction.activity.leftfade;
 import com.mengdd.transactionani.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class FirstActivity extends Activity {
 
@@ -11,5 +14,18 @@ public class FirstActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leftfade_layout);
+
+        View nextButton = findViewById(R.id.next);
+        nextButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in,
+                        R.anim.fade_out_left);
+            }
+        });
     }
 }
--- src/com/mengdd/transactionani/HelloTransactionAnimationMainActivity.java
+++ src/com/mengdd/transactionani/HelloTransactionAnimationMainActivity.java
@@ -10,60 +10,64 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.mengdd.transaction.activity.TestActivities;
+import com.mengdd.transaction.activity.leftfade.FirstActivity;
 import com.mengdd.transaction.fragment.high.TestFragmentActivity;
 import com.mengdd.transaction.fragment.v4.TestFragmentV4Activity;
 
 public class HelloTransactionAnimationMainActivity extends Activity {
 
-	private ListView mListView = null;
-	private static Sample[] mSamples = null;
+    private ListView mListView = null;
+    private static Sample[] mSamples = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		initSamples();
-		mListView = (ListView) findViewById(R.id.list);
-		mListView.setAdapter(new ArrayAdapter<Sample>(this,
-				R.layout.my_list_item, R.id.text, mSamples));
+        initSamples();
+        mListView = (ListView) findViewById(R.id.list);
+        mListView.setAdapter(new ArrayAdapter<Sample>(this,
+                R.layout.my_list_item, R.id.text, mSamples));
 
-		mListView.setOnItemClickListener(new OnItemClickListener() {
+        mListView.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				startActivity(new Intent(HelloTransactionAnimationMainActivity.this,
-						mSamples[position].activityClass));
-			}
-		});
-	}
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                startActivity(new Intent(
+                        HelloTransactionAnimationMainActivity.this,
+                        mSamples[position].activityClass));
+            }
+        });
+    }
 
-	private void initSamples() {
-		mSamples = new Sample[] {
+    private void initSamples() {
+        mSamples = new Sample[] {
 
-				new Sample(R.string.fragment_test, TestFragmentV4Activity.class),
-				new Sample(R.string.fragment_test_high,
-						TestFragmentActivity.class),
-				new Sample(R.string.activity_test, TestActivities.class),
+                new Sample(R.string.fragment_test, TestFragmentV4Activity.class),
+                new Sample(R.string.fragment_test_high,
+                        TestFragmentActivity.class),
+                new Sample(R.string.activity_test, TestActivities.class),
+                new Sample(R.string.activity_test_left_fade,
+                        FirstActivity.class)
 
-		};
-	}
+        };
+    }
 
-	private class Sample {
-		private final CharSequence title;
-		private final Class<? extends Activity> activityClass;
+    private class Sample {
+        private final CharSequence title;
+        private final Class<? extends Activity> activityClass;
 
-		public Sample(int titleResId, Class<? extends Activity> activityClass) {
-			this.activityClass = activityClass;
-			this.title = getResources().getString(titleResId);
+        public Sample(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
 
-		}
+        }
 
-		@Override
-		public String toString() {
-			return title.toString();
-		}
-	}
+        @Override
+        public String toString() {
+            return title.toString();
+        }
+    }
 
 }
